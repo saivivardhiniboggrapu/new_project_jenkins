6c9326a43092fe310508a588919aedb4b609c606
Ubuntu <ubuntu@ip-172-31-28-61.eu-north-1.compute.internal>
2026-02-18 09:47:20 +0000

    this file contain nothing

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "us-east-1"
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0" # Amazon Linux 2 AMI
-  instance_type = "t2.micro"
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
 
-  tags = {
-    Name = "Terraform-VM"
-  }
+  tags = {
+    Name = "Terraform-VM"
+  }
 }
